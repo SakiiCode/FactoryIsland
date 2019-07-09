639278ffebd53fc1dcb6064074f444610970cad4
SakiiCode <sakiicode10@gmail.com>
2019-07-09 21:11:38 +0200

    changed getSelectedStack() to a cached SelectedStack

--- src/ml/sakii/factoryisland/items/PlayerInventory.java
+++ src/ml/sakii/factoryisland/items/PlayerInventory.java
@@ -13,10 +13,11 @@ public class PlayerInventory {
 	public final ConcurrentHashMap<ItemType, Integer> items = new ConcurrentHashMap<>();
 	public static final PlayerInventory Creative = new CreativeInventory();
 	//public Entry<ItemType, Integer> SelectedStack = null;
-	private ItemStack SelectedStack = new ItemStack();
-	public int hotbarIndex = -1;
+	public ItemStack SelectedStack = new ItemStack();
+	private int hotbarIndex = -1;
 	GameEngine engine;
 	boolean activateOnFirst=true;
+	String SelectedName="";
 	
 	
 	public PlayerInventory(GameEngine engine) {
@@ -44,9 +45,9 @@ public class PlayerInventory {
 			if(originalSize==0 && items.size()==1){ //üres volt, nem lett üres
 				if(activateOnFirst && Main.GAME != null) {
 					if(Main.GAME.remoteInventory != null && Main.GAME.remoteInventory.getInv().items.size()==0) {
-						hotbarIndex = 0;
+						setHotbarIndex(0);
 					}else if(Main.GAME.remoteInventory==null) {
-						hotbarIndex = 0;	
+						setHotbarIndex(0);	
 					}
 					//SelectedStack = items.entrySet().toArray(new Entry<ItemType, Integer>[0])[0];
 				}
@@ -59,7 +60,7 @@ public class PlayerInventory {
 				//ItemStack selected = getSelectedStack(); 
 				//if(selected != null && kind==selected.kind) {
 					if(hotbarIndex==items.size()-1){
-						hotbarIndex--;
+						setHotbarIndex(hotbarIndex-1);
 					}
 					/*if(hotbarIndex>-1){
 						SelectedStack = items.get(hotbarIndex);
@@ -71,7 +72,7 @@ public class PlayerInventory {
 				items.remove(kind);
 				
 				if(items.size()==0){ //nem volt üres, üres lett
-					hotbarIndex=-1;
+					setHotbarIndex(-1);
 					if(Main.GAME.remoteInventory != null) {
 						Main.GAME.SwapInv();
 					}
@@ -96,17 +97,9 @@ public class PlayerInventory {
 		return null;
 	}*/
 	
-	public ItemStack getSelectedStack(){
-		if(hotbarIndex>-1) { //BlockInventorynál ez kell 
-			@SuppressWarnings("unchecked")
-			Entry<ItemType,Integer> stack =  (Entry<ItemType,Integer>)(items.entrySet().toArray()[hotbarIndex]);
-			if(stack != null) {
-				SelectedStack.set(stack.getKey(), stack.getValue());
-				return SelectedStack;//new ItemStack(stack.getKey(), stack.getValue());
-			}
-		}
-		return null;
-	}
+	/*public ItemStack getSelectedStack(){
+		return SelectedStack;
+	}*/
 	
 	public int getStack(ItemType kind){
 		/*for(ItemStack is : items){
@@ -118,20 +111,37 @@ public class PlayerInventory {
 		return count==null ? 0 : count;
 	}
 
+	public int getHotbarIndex() {
+		return hotbarIndex;
+	}
 	
+	public void setHotbarIndex(int hotbarIndex) {
+		this.hotbarIndex=hotbarIndex;
+		if(hotbarIndex>-1) { //BlockInventorynál ez kell 
+			@SuppressWarnings("unchecked")
+			Entry<ItemType,Integer> stack =  (Entry<ItemType,Integer>)(items.entrySet().toArray()[hotbarIndex]);
+			if(stack != null) {
+				SelectedStack.set(stack.getKey(), stack.getValue());
+			}
+		}else {
+			SelectedStack = ItemStack.EMPTY;
+		}
+	}
 	
+
 	public void clear() {
 		items.clear();
 		//SelectedStack=null;
-		hotbarIndex=-1;
+		//hotbarIndex=-1;
+		setHotbarIndex(-1);
 	}
 	
 	public void wheelUp(){
 		if(hotbarIndex>-1){
 			if(hotbarIndex == items.size()-1){
-				hotbarIndex = 0;
+				setHotbarIndex(0);
 			}else if(hotbarIndex<items.size()-1) {
-					hotbarIndex++;
+					setHotbarIndex(hotbarIndex+1);
 			}
 			//SelectedStack = items.get(hotbarIndex);
 		}
@@ -142,9 +152,9 @@ public class PlayerInventory {
 		if(hotbarIndex>-1){
 
 			if(hotbarIndex == 0){
-				hotbarIndex = items.size()-1;
+				setHotbarIndex(items.size()-1);
 			}else{
-				hotbarIndex--;
+				setHotbarIndex(hotbarIndex-1);
 			}
 			//SelectedStack = items.get(hotbarIndex);
 		}
