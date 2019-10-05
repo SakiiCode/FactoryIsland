7a673a9326adf37e0b4cb4cef8e82c8d7033a93a
SakiiCode <sakiicode10@gmail.com>
2019-10-05 23:10:07 +0200

    Fluid tipus a folyadekoknak

--- src/ml/sakii/factoryisland/blocks/WaterBlock.java
+++ src/ml/sakii/factoryisland/blocks/WaterBlock.java
@@ -1,184 +1,20 @@
 package ml.sakii.factoryisland.blocks;
 
-import java.util.Map.Entry;
-
 import ml.sakii.factoryisland.GameEngine;
 import ml.sakii.factoryisland.Main;
-import ml.sakii.factoryisland.Polygon3D;
 
-public class WaterBlock extends Block implements TickListener, LoadListener, MetadataListener, BreakListener{
+public class WaterBlock extends Fluid {
 
 
 	
 	public WaterBlock(int x, int y, int z, GameEngine engine) {
-		super("Water",x, y, z,Main.waters[4], Main.waters[4], Main.waters[4], Main.waters[4], Main.waters[4], Main.waters[4],engine);
-
-		BlockMeta.put("height", "4");
-		transparent=true;
-		solid = false;
-		refreshRate = 10;
+		this(x,y,z,4,engine);
 	}
 	
 	public WaterBlock(int x, int y, int z, int height, GameEngine engine) {
-		super("Water",x, y, z,Main.waters[height], Main.waters[height], Main.waters[height], Main.waters[height], Main.waters[height], Main.waters[height],engine);
-
-		BlockMeta.put("height", height+"");
-		transparent=true;
-		solid = false;
-		refreshRate = 10;
+		super("Water",x, y, z,height,Main.waters,engine);
 
 	}
 	
-	
-
-	public void setHeight(int newHeight){
-		setMetadata("height",""+newHeight, true);
-		heightMap();
-		
-	}
-	
-
-	private void heightMap() {
-		for(Polygon3D polygon : Polygons){
-			polygon.s = Main.waters[getHeight()];
-			polygon.recalcLightedColor();
-		}
-	}
-
-	@Override
-	public boolean onMetadataUpdate(String key, String value)
-	{
-		BlockMeta.put(key, value);
-		if(key.equals("height")){
-			heightMap();
-		}
-		
-		return true;
-	}
-
-
-	
-	@Override
-	public void onLoad(){
-		heightMap();
-	}
-
-	
-	@Override
-	public boolean tick(long tickCount) {
-
-			boolean hasEmptyNearby=false,hasPlusOneNearby=false,hasBiggerNearby=false;
-			int biggerheight=0;
-			int height = getHeight();
-			for(Entry<BlockFace,Block> entry : get6Blocks(this,true).entrySet()){
-				BlockFace key = entry.getKey();
-				Block value = entry.getValue();
-				
-				if(key != BlockFace.BOTTOM && key != BlockFace.TOP){
-					if(value == Block.NOTHING){
-						hasEmptyNearby = true;
-					}else if(value instanceof WaterBlock){
-						//WaterBlock wb = (WaterBlock)value;
-						int otherHeight = value.getHeight();
-						
-						if(otherHeight == height+1){
-							hasPlusOneNearby=true;
-						}else if(otherHeight > height + 1){
-							if(biggerheight<otherHeight){
-								biggerheight=otherHeight;
-								
-								hasBiggerNearby=true;
-							}
-							
-						}
-						
-						
-					}
-					
-					
-				}
-				
-			}
-			
-			
-			if(hasBiggerNearby){
-				if(height != 4){
-					setHeight(biggerheight-1);
-				
-				}
-			}else if(!hasPlusOneNearby){
-				if(height != 4){
-					if(height == 1){
-						//deleteBlock(this);
-						Engine.world.destroyBlock(this, true);
-
-						return false;
-					}
-					setHeight(height-1);
-				}
-				
-			}
-			
-			if(hasEmptyNearby){
-				
-				
-				if(getHeight() > 1){
-					for(Entry<BlockFace, Block> entry : get6Blocks(this, true).entrySet()){
-						BlockFace key = entry.getKey();
-						Block value = entry.getValue();
-						if(key != BlockFace.TOP && key != BlockFace.BOTTOM && value == Block.NOTHING){
-							
-							//WaterBlock water1 = new WaterBlock(x + key.direction[0], y + key.direction[1], z + key.direction[2], getHeight()-1,game, server);
-							
-							OldBlock temp = new OldBlock(x + key.direction[0], y + key.direction[1], z + key.direction[2], Engine);
-							int biggestyet = 0;
-							for(Entry<BlockFace, Block> wentry : get6Blocks(temp, false).entrySet()){
-								BlockFace wkey = wentry.getKey();
-								Block wvalue = wentry.getValue();
-								if(wkey != BlockFace.TOP && wkey != BlockFace.BOTTOM){
-									if(wvalue.name.equals("Water")){
-										WaterBlock wb = (WaterBlock)wvalue;
-										int wotherHeight = wb.getHeight();
-										if(wotherHeight > height + 1 && biggestyet < wotherHeight){
-											biggestyet=wotherHeight;
-											//water1.setHeight(wotherHeight - 1);
-											//water1.BlockMeta.put("height", ""+(wotherHeight - 1));
-											//break;
-										}
-									}
-								}
-							}
-							WaterBlock water1;
-
-							if(biggestyet==0){
-								water1 = new WaterBlock(x + key.direction[0], y + key.direction[1], z + key.direction[2], height-1,Engine);
-							}else{
-								water1 = new WaterBlock(x + key.direction[0], y + key.direction[1], z + key.direction[2], biggestyet-1,Engine);
-
-							}
-							Engine.world.addBlockNoReplace(water1, true);
-
-						}
-					}
-				}
-				
-			}
-			
-			
-			
-			return false;
-			
-
-			
-	}
-
-	@Override
-	public boolean breaked(String username)
-	{
-		if(getHeight()==4) {
-			return true;
-		}
-		return false;
-	}
 }
 
