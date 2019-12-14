991135d22d69a788dc3afef78130e12dbf82be68
SakiiCode <sakiicode10@gmail.com>
2019-12-14 14:34:33 +0100

    gameclient fuggvenyekkel

--- src/ml/sakii/factoryisland/net/GameClient.java
+++ src/ml/sakii/factoryisland/net/GameClient.java
@@ -20,7 +20,6 @@ import ml.sakii.factoryisland.Vector;
 import ml.sakii.factoryisland.blocks.Block;
 import ml.sakii.factoryisland.blocks.BlockInventoryInterface;
 import ml.sakii.factoryisland.entities.Entity;
-import ml.sakii.factoryisland.entities.PlayerMP;
 
 
 public class GameClient extends Thread{
@@ -29,7 +28,7 @@ public class GameClient extends Thread{
 			"00", "02", "05", "06", "07", "66", "67" ,"98", "01", "08", "03","10","11","12", "loaded", "ping", "pong"));*/
 	
 	static final Set<String> ALLCODES = new HashSet<>(Arrays.asList(
-			"00", "01", "02", "03","04", "05", "06", "07", "08","10","11", "14","15","16","17","97", "13", "66", "67" ,"98", "loaded", "ping", "pong"));
+			"00", "01", "02", "03","04", "05", "06", "07", "08","10","11", "14","15","16","17","97", "13", "66", "67" ,"98","18", "loaded", "ping", "pong"));
 	
 	//static final String DELIMETER = "\r\n";
 	static final long CMDTIME = 20; 
@@ -158,15 +157,16 @@ public class GameClient extends Thread{
 	private String handleMessage(String message) {
 		String[] part = message.split(",");
 		String otherName = (part.length>1) ? part[1] : "";
-		long ID;
 		switch(part[0]){
 		
 		case "00": // PROTOCOL VERSION CHECK
 			if(!part[1].equals(""+Connection.PROTOCOL_VERSION)) {
 				return "Incompatible server version: "+part[1] + "(current:"+Connection.PROTOCOL_VERSION+")";
 			}
+			game.Engine.world.Entities.remove(game.PE.ID);
 			game.PE.ID = Long.parseLong(part[2]);
-			
+			game.Engine.world.Entities.put(game.PE.ID, game.PE);
+			//game.Engine.world.addEntity(game.PE, false);
 			break;
 		
 		case "01": // DOWNLOAD BLOCKS
@@ -201,11 +201,11 @@ public class GameClient extends Thread{
 			
 			
 			break;
-		case "03": // ADD A PLAYER 03,x,y,z,yaw,ID
+		/*case "03": // ADD A PLAYER 03,x,y,z,yaw,ID (unused)
 			
 				Vector ViewFrom = new Vector(Float.parseFloat(part[2]), Float.parseFloat(part[3]), Float.parseFloat(part[4]));
 				ID = Long.parseLong(part[6]);
-				PlayerMP newPlayer = new PlayerMP(ViewFrom, new EAngle(Float.parseFloat(part[5]), 0), part[1],ID, game.Engine);
+				PlayerMP newPlayer = new PlayerMP(ViewFrom, new EAngle(Float.parseFloat(part[5]), 0), part[1],20,ID, game.Engine);
 				game.playerList.put(part[1], newPlayer);
 				game.Objects.addAll(newPlayer.Objects);
 				//game.Objects.add(newPlayer.title);
@@ -215,29 +215,33 @@ public class GameClient extends Thread{
 			
 			break;
 		
-		case "04": // MOVE A PLAYER
+		case "04": // MOVE A PLAYER (unused)
 			
 			PlayerMP otherPlayer = game.playerList.get(otherName);
 			if(otherPlayer!=null) {
 				otherPlayer.ViewFrom.x = Float.parseFloat(part[2]);
 				otherPlayer.ViewFrom.y = Float.parseFloat(part[3]);
 				otherPlayer.ViewFrom.z = Float.parseFloat(part[4]);
-				otherPlayer.yaw = Float.parseFloat(part[5]);
+				otherPlayer.ViewAngle.yaw = Float.parseFloat(part[5]);
 				otherPlayer.update();
 			}
 			
 			break;
-		
+		*/
 		case "67": // DELETE PLAYER
-			
-			game.Objects.removeAll(game.playerList.get(otherName).Objects);
+			for(Entity e : game.Engine.world.getAllEntities()) {
+				if(e.name.equals(otherName)) {
+					game.Engine.world.Entities.remove(e.ID);
+					game.Objects.removeAll(e.Objects);
+				}
+			}
+			//game.Objects.removeAll(game.playerList.get(otherName).Objects);
 			//game.Entities.remove(game.playerList.get(otherName));
-			game.playerList.remove(otherName);
+			//game.playerList.remove(otherName);
 			
 			break;
 		
 		case "05": // PLACE BLOCK
-			
 			receiveBlockPlace(part);
 			break;
 		case "06": // DELETE BLOCK
@@ -245,33 +249,16 @@ public class GameClient extends Thread{
 			break;
 		case "07": // EDIT METADATA
 			receiveMetadataEdit(part);
-
 			break;
 		case "10": // ADD TO INVENTORY
-			//int size0 = game.Engine.Inv.items.size();
-			game.Engine.Inv.add(Main.Items.get(part[2]), cInt(part[3]), false);
-			/*if(game.Engine.Inv.items.size()==0) TODO ezek a switchinventoryk kellhetnek
-				game.SwitchInventory(false);
-			if(game.Engine.Inv.items.size()==1 && size0==0)
-				game.SwitchInventory(true);*/
+			receiveInvPlayerAdd(part);
 			break;
 		case "13": // ADD TO BLOCK INVENTORY
-			((BlockInventoryInterface)game.Engine.world.getBlockAt(cInt(part[1]), cInt(part[2]), cInt(part[3]))).getInv().add(Main.Items.get(part[4]), cInt(part[5]), false);
-			/*if(game.remoteInventory.getInv().items.size()==0)
-				game.SwitchInventory(true);*/
+			receiveInvBlockAdd(part);
 			break;
 		case "14": // SWAP BLOCKS 14,Sakii,1,2,3,Stone,true (add to local)
-			boolean addToLocal = Boolean.parseBoolean(part[6]);
-			game.Engine.Inv.add(Main.Items.get(part[5]), addToLocal ? 1 : -1, false);
-			((BlockInventoryInterface)game.Engine.world.getBlockAt(cInt(part[2]), cInt(part[3]), cInt(part[4]))).getInv().add(Main.Items.get(part[5]), addToLocal ? -1 : 1, false);
-			
-			/*if(game.remoteInventory.getInv().items.size()==0) {
-				game.SwitchInventory(true);
-			}else if(game.Engine.Inv.items.size()==0) {
-				game.SwitchInventory(false);
-			}*/
+			receiveInvSwap(part);
 			break;
-
 		case "98": // SERVER CLOSED
 			if(game.Engine.server == null) {
 				//JOptionPane.showMessageDialog(Main.Frame.getContentPane(), "Server closed", "Disconnected", JOptionPane.ERROR_MESSAGE);
@@ -281,14 +268,15 @@ public class GameClient extends Thread{
 			
 			break;
 		case "97": // SAME USERNAME
-				//JOptionPane.showInternalMessageDialog(Main.Frame.getContentPane(), "Someone with the same username already logged in", "Disconnected", JOptionPane.ERROR_MESSAGE);
-				//game.disconnect();
-				return "Someone with the same username already logged in";
+			//JOptionPane.showInternalMessageDialog(Main.Frame.getContentPane(), "Someone with the same username already logged in", "Disconnected", JOptionPane.ERROR_MESSAGE);
+			//game.disconnect();
+			return "Someone with the same username already logged in";
 			//break;
-		case "11": //FORCE MOVE
+		case "11": //FORCE MOVE ON CONNECT
 			game.PE.move(Float.parseFloat(part[1]), Float.parseFloat(part[2]), Float.parseFloat(part[3]), false);
 			game.PE.ViewAngle.yaw = Float.parseFloat(part[4]);
 			game.PE.ViewAngle.pitch = Float.parseFloat(part[5]);
+			game.PE.setHealth((int)Float.parseFloat(part[6]));
 			game.moved=true;
 			break;
 		case "15": // SPAWN ENTITY 15,className,x,y,z,yaw,pitch,name,health,ID
@@ -300,9 +288,9 @@ public class GameClient extends Thread{
 		/*case "17": // KILL ENTITIY
 			receiveEntityKill(part);
 			break;*/
+		case "18": // HURT ENTITY
+			receiveEntityHurt(part);
 			break;
-		case "18":
-			game.Engine.world.getEntity(Long.parseLong(part[1])).hurt(Integer.parseInt(part[2]),false);
 		case "pong":
 			Main.log("ping time: " + (System.currentTimeMillis()-pingTime) + " ms");
 			break;
@@ -355,8 +343,7 @@ public class GameClient extends Thread{
 	}
 	
 	public void sendMetadataEdit(Block b, String key, String value) {
-		sendData(("07," + b.x + "," + b.y + "," + b.z + ","
-				+ key + "," + value));
+		sendData(("07," + b.x + "," + b.y + "," + b.z + ","	+ key + "," + value));
 	}
 	
 	void receiveMetadataEdit(String[] part) {
@@ -366,8 +353,12 @@ public class GameClient extends Thread{
 		}
 	}
 	
+	public static String constructEntityCreate(Entity e) {
+		return "15,"+e.className+","+e.getPos()+","+e.ViewAngle.yaw+","+e.ViewAngle.pitch+","+e.name+","+e.getHealth()+","+e.ID;
+	}
+	
 	public void sendEntityCreate(Entity e) {
-		sendData("15,"+e.className+","+e.getPos()+","+e.ViewAngle.yaw+","+e.ViewAngle.pitch+","+e.name+","+e.ID);
+		sendData(constructEntityCreate(e));
 	}
 	
 	void receiveEntityCreate(String[] part) {
@@ -379,21 +370,35 @@ public class GameClient extends Thread{
 		long ID=Long.parseLong(part[9]);
 		
 		Entity e = Entity.createEntity(className, pos, aim, name,health, ID, game.Engine); 
-		game.Engine.world.addEntity(e);
+		game.Engine.world.addEntity(e, false);
+		/*if(e instanceof PlayerMP && e.ID != game.PE.ID) {
+			game.playerList.put(name, (PlayerMP)e);
+		}*/
 	}
 	
-	public void sendEntityMove(Entity e) {
-		sendData("16,"+e.ID+","+e.getPos().x+","+e.getPos().y+","+e.getPos().z); 
+	public static String constructEntityMove(long ID, float x, float y, float z, float yaw, float pitch) {
+		return "16,"+ID+","+x+","+y+","+z+","+yaw+","+pitch; 
+	}
+	
+	
+	public void sendEntityMove(long ID, float x, float y, float z, float yaw, float pitch) {
+		sendData(constructEntityMove(ID,x, y, z, yaw, pitch)); 
 	}
 	
 	void receiveEntityMove(String[] part) {
-		for(int i=1;i<part.length;i+=4) {
-			Entity en = game.Engine.world.getEntity(Long.parseLong(part[i]));
+		//for(int i=1;i<part.length;i+=4) {
+		if(part.length==7) {
+			Entity en = game.Engine.world.getEntity(Long.parseLong(part[1]));
 			if(en!=null) {
-				en.move(Float.parseFloat(part[i+1]), Float.parseFloat(part[i+2]), Float.parseFloat(part[i+3]), false);
+				en.move(Float.parseFloat(part[2]), Float.parseFloat(part[3]), Float.parseFloat(part[4]), false);
+				en.ViewAngle.yaw = Float.parseFloat(part[5]); //TODO arrayindexoutofboundsexception
+				en.ViewAngle.pitch = Float.parseFloat(part[6]);
 				en.update();
 			}
+		}else {
+			Main.err("EntityMove shorter:"+Arrays.toString(part));
 		}
+		//}
 	}
 	
 	public void sendEntityHurt(long ID, int points) {
@@ -402,23 +407,31 @@ public class GameClient extends Thread{
 	}
 	
 	void receiveEntityHurt(String[] part) {
-		
+		game.Engine.world.hurtEntity(Long.parseLong(part[1]),Integer.parseInt(part[2]),false);
 	}
 	
-	public void sendEntityKill(long ID) {
+	/*public void sendEntityKill(long ID) {
 		sendData("17,"+ID);
 	}
 	
 	void receiveEntityKill(String[] part) {
-		game.Engine.world.killEntity(Long.parseLong(part[1]), false);
-	}
+		long ID = Long.parseLong(part[1]);
+		game.Engine.world.killEntity(ID, false);
+		if(ID == game.PE.ID) {
+			game.notifyDeath();
+		}
+	}*/
 	
 	
 	public void sendInvSwap(String username, Block b, String itemName, boolean addToLocal) {
 		 // 14,Sakii,1,2,3,Stone,true (add to local)
-		sendData(
-				"14," + username + "," + b.x + "," + b.y
-						+ "," + b.z + "," + itemName + "," + addToLocal);
+		sendData("14," + username + "," + b.x + "," + b.y + "," + b.z + "," + itemName + "," + addToLocal);
+	}
+	
+	void receiveInvSwap(String[] part) {
+		boolean addToLocal = Boolean.parseBoolean(part[6]);
+		game.Engine.Inv.add(Main.Items.get(part[5]), addToLocal ? 1 : -1, false);
+		((BlockInventoryInterface)game.Engine.world.getBlockAt(cInt(part[2]), cInt(part[3]), cInt(part[4]))).getInv().add(Main.Items.get(part[5]), addToLocal ? -1 : 1, false);
 	}
 	
 	public void sendInvBlockAdd(String username, Block b, String name, int amount) {
@@ -426,12 +439,22 @@ public class GameClient extends Thread{
 
 	}
 	
+	void receiveInvBlockAdd(String[] part) {
+		((BlockInventoryInterface)game.Engine.world.getBlockAt(cInt(part[1]), cInt(part[2]), cInt(part[3]))).getInv().add(Main.Items.get(part[4]), cInt(part[5]), false);
+	}
+	
 	public void sendInvPlayerAdd(String username, String name, int amount) {
 		sendData("10,"+username+","+name+","+amount);
 	}
 	
+	void receiveInvPlayerAdd(String[] part) {
+		game.Engine.Inv.add(Main.Items.get(part[2]), cInt(part[3]), false);
+	}
+	
 	public void sendPlayerPos() {
-		sendEntityMove(game.PE);
+		Vector pos = game.PE.getPos();
+		EAngle aim = game.PE.ViewAngle;
+		sendEntityMove(game.PE.ID, pos.x, pos.y, pos.z, aim.yaw, aim.pitch);
 		//sendData("04," + username + "," + x + "," + y + "," + z + "," + yaw +"," + pitch);
 	}
 	
@@ -454,8 +477,11 @@ public class GameClient extends Thread{
 			//outToClient.flush();
 			
 			if(Main.devmode) {
-				if(ALLCODES.contains(data.split(",")[0])){
-					Main.log("(CLIENT:"+Config.username+") SENT:      "+data);
+				String code = data.split(",")[0];
+				if(ALLCODES.contains(code)){
+					//if(!code.equals("16")) {
+						Main.log("(CLIENT:"+Config.username+") SENT:      "+data);
+					//}
 				}else if(!data.isEmpty()){
 					Main.log("(CLIENT:"+Config.username+") I DUNNO WAT I SENT LOL:  "+data);
 				}
@@ -484,7 +510,6 @@ public class GameClient extends Thread{
 				lock.wait();
 			} catch (InterruptedException e)
 			{
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			
