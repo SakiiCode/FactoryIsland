7ec46b21c1958e560ceea1eb48b9c2417dab5fe9
SakiiCode <sakiicode10@gmail.com>
2023-08-29 22:28:58 +0200

    stars blinking, color, size adjustments

--- src/main/java/ml/sakii/factoryisland/Star.java
+++ src/main/java/ml/sakii/factoryisland/Star.java
@@ -3,45 +3,55 @@ package ml.sakii.factoryisland;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
-public class Star extends Object3D{
-	final Vector pos;
+
+public class Star extends Object3D {
 	
+	private static final float MAX_SIZE=5f;
+	
+	final Vector pos;
+
 	private final float size;
 	private final Point p = new Point();
+	private final Color4 c;
 	
+
 	public Star() {
-		pos = new Vector(
-				(float)(Math.random()*2-1),
-				(float)(Math.random()*2-1),
-				(float)(Math.random()*2-1));
-		size = (float)(Math.random()*10);
-		AvgDist=1000;
-	}
-	
-	public Star(int size) {
-		pos = new Vector(1,0,0.5f);
-		this.size = size;
+		float x = (float) (Math.random() * 2 - 1);
+		float y = (float) (Math.random() * 2 - 1);
+		float z = (float) (Math.random() * 2 - 1);
+		pos = new Vector(x, y, z);
+		size = (float) (Math.random() * MAX_SIZE);
+		AvgDist = 1000;
+		c = new Color4((float)(0.5+Math.random()*0.5), (float)(0.5+Math.random()*0.5), (float) (0.5+Math.random()*0.5f), size/MAX_SIZE);
 	}
 
-	
-	//TODO data-oriented
+	public Star(int size) {
+		pos = new Vector(1, 0, 0.5f);
+		this.size = size;
+		c= new Color4(Color.WHITE);
+	}
+
+	// TODO data-oriented
 	@Override
 	protected boolean update(Game game, Vector[][] clip2, double[][][] clipUV2, Vector tmpVector) {
+		if (game.ViewVector.DotProduct(pos) > 0 || (size < MAX_SIZE && Math.random() < 0.001f)) {
+			return false;
+		}
+
 		tmpVector.set(pos).add(game.PE.getPos());
-		game.convert3Dto2D(tmpVector,p);
-		return game.ViewVector.DotProduct(pos) > 0;
+		game.convert3Dto2D(tmpVector, p);
+		return true;
 	}
 
 	@Override
 	protected void draw(Graphics g, Game game) {
-		int size = (int)(Config.resolutionScaling*this.size);
-		if(Config.fogEnabled) {
+		int size = (int) (Config.resolutionScaling * this.size);
+		if (Config.fogEnabled) {
 			g.setColor(Color.GRAY);
-		}else {
-			g.setColor(Color.white);
+		} else {
+			g.setColor(c.getColor());
 		}
-		g.fillOval(p.x, p.y, size, size);		
+		g.fillOval(p.x, p.y, size, size);
 	}
-	
-	
+
 }
